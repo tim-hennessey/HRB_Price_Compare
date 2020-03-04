5c1496af5c4a7c598daafe942c6985593dbe284d
tim.hennessey <Tim.Hennessey@deutschinc.com>
2020-03-04 15:23:12 -0800

    Versions

---
+++ app/130_DIY_DIYPRICE_H175_A1_C4_T11_300x250_ALL_JS/scripts/Animation.js
@@ -0,0 +1,93 @@
+var app = app || {};
+
+
+app.Animation = (function () {
+
+	var banner = document.getElementById('banner');
+	var t = TweenMax;
+	var tl1 = new TimelineMax();
+	var tl2 = new TimelineMax({paused:true});
+
+	var cta = document.getElementById('cta');
+	var cta_txt = document.getElementsByClassName('cta_txt');
+	var buttonExit = document.getElementById('button-exit');
+
+
+	// --------------------------------------------------------------------------------------
+	// set default properties
+	function initialize() {
+		// DO NOT EDIT: reveals banner once loaded
+		t.set(banner, {opacity:1});
+
+		buttonExit.addEventListener('mouseover', function () {
+            t.to(cta, .25, {backgroundColor: "#3bc0f8"});
+            t.to(cta_txt, .25, {fill: "#262626" });
+        });
+
+        buttonExit.addEventListener('mouseout', function () {
+            t.to(cta, .25, {backgroundColor: "#f5cc02"});
+            t.to(cta_txt, .25, {fill: "#000" });
+        });
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Starts the animation
+	function start() {
+
+
+		tl1.set("#outline", {display:"block"}, "+=.5")
+		.set("#outline", {display:"none"}, "+=.5")
+		.set("#outline", {display:"block"}, "+=.5")
+		.set("#outline", {display:"none"}, "+=.5")
+		.set("#outline", {display:"block"}, "+=.5")
+		.set("#outline", {display:"none"}, "+=.5")
+		.set("#solid", {display:"none"})
+
+		.to(tl2, 2, {progress:1, ease: Sine.easeInOut}, "+=.5");
+
+		tl2.set("#_1", {display:"block"})
+		.set("#_2", {display:"block"}, "+=.1")
+		.set("#_3", {display:"block"}, "+=.1")
+		.set("#_4", {display:"block"}, "+=.1")
+		.set("#_5", {display:"block"}, "+=.1")
+		.set("#_6", {display:"block"}, "+=.1")
+		.set("#_7", {display:"block"}, "+=.1")
+		.set("#_8", {display:"block"}, "+=.1")
+		.set("#_9", {display:"block"}, "+=.1")
+		.set("#_10", {display:"block"}, "+=.1")
+		.set("#_11", {display:"block"}, "+=.1")
+		.set("#_12", {display:"block"}, "+=.1")
+		.set("#_13", {display:"block"}, "+=.1")
+		.set("#_14", {display:"block"}, "+=.1")
+		.set("#_15", {display:"block"}, "+=.1")
+		.set("#_16", {display:"block"}, "+=.1")
+		.set("#_17", {display:"block"}, "+=.1")
+		.set("#_18", {display:"block"}, "+=.1")
+		.set("#_19", {display:"block"}, "+=.1")
+		.set("#_20", {display:"block"}, "+=.1");
+		
+	}
+
+
+	// function animateIcon() {
+	// 	var img = document.getElementById('gif');
+	// 	var newImg = new Image;
+	// 	newImg.onload = function() { img.src = this.src; }
+	// 	newImg.src = 'images/gif.gif?';
+	// }
+
+	// --------------------------------------------------------------------------------------
+	// Stops the animation
+	function stop() {
+		console.log("stopping animation");
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Publicly accessible methods and properties
+	return {
+		initialize:initialize,
+		start:start,
+		stop:stop
+	}
+
+})();
---
+++ app/130_DIY_DIYPRICE_H176_A1_C4_T11_160x600_ALL_JS/scripts/Animation.js
@@ -0,0 +1,53 @@
+var app = app || {};
+
+
+app.Animation = (function () {
+
+	var banner = document.getElementById('banner');
+	var t = TweenMax;
+	var tl = new TimelineMax({repeat:-1});
+	var cta = document.getElementById('cta');
+	var cta_txt = document.getElementsByClassName('cta_txt');
+	var buttonExit = document.getElementById('button-exit');
+
+	// --------------------------------------------------------------------------------------
+	// set default properties
+	function initialize() {
+		// DO NOT EDIT: reveals banner once loaded
+		t.set(banner, {opacity:1});
+
+		buttonExit.addEventListener('mouseover', function () {
+            t.to(cta, .25, {backgroundColor: "#3bc0f8"});
+            t.to(cta_txt, .25, {fill: "#262626" });
+        });
+
+        buttonExit.addEventListener('mouseout', function () {
+            t.to(cta, .25, {backgroundColor: "#f5cc02"});
+            t.to(cta_txt, .25, {fill: "#000" });
+        });
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Starts the animation
+	function start() {
+
+		tl.set("#overlay", {display:"none"}, "+=.5")
+		.set("#overlay", {display:"block"}, "+=.5");
+		
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Stops the animation
+	function stop() {
+		console.log("stopping animation");
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Publicly accessible methods and properties
+	return {
+		initialize:initialize,
+		start:start,
+		stop:stop
+	}
+
+})();
